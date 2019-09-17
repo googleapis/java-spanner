googleapis/java-spanner

commit b3c50edec74efa7891e6f1e8e762996c79bee75a

Spanner: Use generic retry helper for transaction retries (#5433)
* use generic retry helper for transaction retries

* check context on retries

* fixed reference to expected exception

google-cloud-spanner/src/main/java/com/google/cloud/spanner/TransactionRunnerImpl.java
@@ -36,7 +36,6 @@ import com.google.spanner.v1.ExecuteSqlRequest;
 import com.google.spanner.v1.ExecuteSqlRequest.QueryMode;
 import com.google.spanner.v1.RollbackRequest;
 import com.google.spanner.v1.TransactionSelector;
-import io.grpc.Context;
 import io.opencensus.common.Scope;
 import io.opencensus.trace.AttributeValue;
 import io.opencensus.trace.Span;
@@ -44,6 +43,8 @@ import io.opencensus.trace.Tracer;
 import io.opencensus.trace.Tracing;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nullable;
@@ -266,16 +267,7 @@ class TransactionRunnerImpl implements SessionTransaction, TransactionRunner {
   }
 
   private boolean blockNestedTxn = true;
-
-  /** Allow for testing of backoff logic */
-  static class Sleeper {
-    void backoffSleep(Context context, long backoffMillis) {
-      SpannerImpl.backoffSleep(context, backoffMillis);
-    }
-  }
-
   private final SessionImpl session;
-  private final TransactionRunnerImpl.Sleeper sleeper;
   private final Span span;
   private TransactionContextImpl txn;
   private volatile boolean isValid = true;
@@ -286,19 +278,9 @@ class TransactionRunnerImpl implements SessionTransaction, TransactionRunner {
     return this;
   }
 
-  TransactionRunnerImpl(
-      SessionImpl session,
-      SpannerRpc rpc,
-      TransactionRunnerImpl.Sleeper sleeper,
-      int defaultPrefetchChunks) {
-    this.session = session;
-    this.sleeper = sleeper;
-    this.span = Tracing.getTracer().getCurrentSpan();
-    this.txn = session.newTransaction();
-  }
-
   TransactionRunnerImpl(SessionImpl session, SpannerRpc rpc, int defaultPrefetchChunks) {
-    this(session, rpc, new Sleeper(), defaultPrefetchChunks);
+    this.session = session;
+    this.span = Tracing.getTracer().getCurrentSpan();
   }
 
   @Nullable
@@ -308,7 +290,6 @@ class TransactionRunnerImpl implements SessionTransaction, TransactionRunner {
       if (blockNestedTxn) {
         SessionImpl.hasPendingTransaction.set(Boolean.TRUE);
       }
-
       return runInternal(callable);
     } catch (RuntimeException e) {
       TraceUtil.endSpanWithFailure(span, e);
@@ -321,96 +302,98 @@ class TransactionRunnerImpl implements SessionTransaction, TransactionRunner {
     }
   }
 
-  private <T> T runInternal(TransactionCallable<T> callable) {
-    BackOff backoff = SpannerImpl.newBackOff();
-    final Context context = Context.current();
-    int attempt = 0;
-    // TODO: Change this to use TransactionManager.
-    while (true) {
-      checkState(
-          isValid, "TransactionRunner has been invalidated by a new operation on the session");
-      SpannerImpl.checkContext(context);
-      attempt++;
-      // TODO(user): When using streaming reads, consider using the first read to begin
-      // the txn.
-      span.addAnnotation(
-          "Starting Transaction Attempt",
-          ImmutableMap.of("Attempt", AttributeValue.longAttributeValue(attempt)));
-      txn.ensureTxn();
+  private <T> T runInternal(final TransactionCallable<T> txCallable) {
+    final AtomicInteger attempt = new AtomicInteger();
+    Callable<T> retryCallable =
+        new Callable<T>() {
+          @Override
+          public T call() {
+            txn = session.newTransaction();
+            checkState(
+                isValid,
+                "TransactionRunner has been invalidated by a new operation on the session");
+            attempt.incrementAndGet();
+            // TODO(user): When using streaming reads, consider using the first read to begin
+            // the txn.
+            span.addAnnotation(
+                "Starting Transaction Attempt",
+                ImmutableMap.of("Attempt", AttributeValue.longAttributeValue(attempt.longValue())));
+            txn.ensureTxn();
 
-      T result;
-      boolean shouldRollback = true;
-      try {
-        result = callable.run(txn);
-        shouldRollback = false;
-      } catch (Exception e) {
-        txnLogger.log(Level.FINE, "User-provided TransactionCallable raised exception", e);
-        if (txn.isAborted() || (e instanceof AbortedException)) {
-          span.addAnnotation(
-              "Transaction Attempt Aborted in user operation. Retrying",
-              ImmutableMap.of("Attempt", AttributeValue.longAttributeValue(attempt)));
-          shouldRollback = false;
-          backoff(context, backoff);
-          continue;
-        }
-        SpannerException toThrow;
-        if (e instanceof SpannerException) {
-          toThrow = (SpannerException) e;
-        } else {
-          toThrow = newSpannerException(ErrorCode.UNKNOWN, e.getMessage(), e);
-        }
-        span.addAnnotation(
-            "Transaction Attempt Failed in user operation",
-            ImmutableMap.<String, AttributeValue>builder()
-                .putAll(TraceUtil.getExceptionAnnotations(toThrow))
-                .put("Attempt", AttributeValue.longAttributeValue(attempt))
-                .build());
-        throw toThrow;
-      } finally {
-        if (shouldRollback) {
-          txn.rollback();
-        }
-      }
+            T result;
+            boolean shouldRollback = true;
+            try {
+              result = txCallable.run(txn);
+              shouldRollback = false;
+            } catch (Exception e) {
+              txnLogger.log(Level.FINE, "User-provided TransactionCallable raised exception", e);
+              if (txn.isAborted() || (e instanceof AbortedException)) {
+                span.addAnnotation(
+                    "Transaction Attempt Aborted in user operation. Retrying",
+                    ImmutableMap.of(
+                        "Attempt", AttributeValue.longAttributeValue(attempt.longValue())));
+                shouldRollback = false;
+                if (e instanceof AbortedException) {
+                  throw (AbortedException) e;
+                }
+                throw SpannerExceptionFactory.newSpannerException(
+                    ErrorCode.ABORTED, e.getMessage(), e);
+              }
+              SpannerException toThrow;
+              if (e instanceof SpannerException) {
+                toThrow = (SpannerException) e;
+              } else {
+                toThrow = newSpannerException(ErrorCode.UNKNOWN, e.getMessage(), e);
+              }
+              span.addAnnotation(
+                  "Transaction Attempt Failed in user operation",
+                  ImmutableMap.<String, AttributeValue>builder()
+                      .putAll(TraceUtil.getExceptionAnnotations(toThrow))
+                      .put("Attempt", AttributeValue.longAttributeValue(attempt.longValue()))
+                      .build());
+              throw toThrow;
+            } finally {
+              if (shouldRollback) {
+                txn.rollback();
+              }
+            }
 
-      try {
-        txn.commit();
-        span.addAnnotation(
-            "Transaction Attempt Succeeded",
-            ImmutableMap.of("Attempt", AttributeValue.longAttributeValue(attempt)));
-        return result;
-      } catch (AbortedException e) {
-        txnLogger.log(Level.FINE, "Commit aborted", e);
-        span.addAnnotation(
-            "Transaction Attempt Aborted in Commit. Retrying",
-            ImmutableMap.of("Attempt", AttributeValue.longAttributeValue(attempt)));
-        backoff(context, backoff);
-      } catch (SpannerException e) {
-        span.addAnnotation(
-            "Transaction Attempt Failed in Commit",
-            ImmutableMap.<String, AttributeValue>builder()
-                .putAll(TraceUtil.getExceptionAnnotations(e))
-                .put("Attempt", AttributeValue.longAttributeValue(attempt))
-                .build());
-        throw e;
-      }
-    }
+            try {
+              txn.commit();
+              span.addAnnotation(
+                  "Transaction Attempt Succeeded",
+                  ImmutableMap.of(
+                      "Attempt", AttributeValue.longAttributeValue(attempt.longValue())));
+              return result;
+            } catch (AbortedException e) {
+              txnLogger.log(Level.FINE, "Commit aborted", e);
+              span.addAnnotation(
+                  "Transaction Attempt Aborted in Commit. Retrying",
+                  ImmutableMap.of(
+                      "Attempt", AttributeValue.longAttributeValue(attempt.longValue())));
+              throw e;
+            } catch (SpannerException e) {
+              span.addAnnotation(
+                  "Transaction Attempt Failed in Commit",
+                  ImmutableMap.<String, AttributeValue>builder()
+                      .putAll(TraceUtil.getExceptionAnnotations(e))
+                      .put("Attempt", AttributeValue.longAttributeValue(attempt.longValue()))
+                      .build());
+              throw e;
+            }
+          }
+        };
+    return SpannerRetryHelper.runTxWithRetriesOnAborted(retryCallable);
   }
 
   @Override
   public Timestamp getCommitTimestamp() {
+    checkState(txn != null, "run() has not yet returned normally");
     return txn.commitTimestamp();
   }
 
   @Override
   public void invalidate() {
     isValid = false;
-  }
-
-  private void backoff(Context context, BackOff backoff) {
-    long delay = txn.getRetryDelayInMillis(backoff);
-    txn = session.newTransaction();
-    span.addAnnotation(
-        "Backing off", ImmutableMap.of("Delay", AttributeValue.longAttributeValue(delay)));
-    sleeper.backoffSleep(context, delay);
   }
 }

google-cloud-spanner/src/test/java/com/google/cloud/spanner/TransactionRunnerImplTest.java
@@ -18,14 +18,11 @@ package com.google.cloud.spanner;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.google.api.client.util.BackOff;
 import com.google.cloud.spanner.TransactionRunner.TransactionCallable;
 import com.google.cloud.spanner.TransactionRunnerImpl.TransactionContextImpl;
 import com.google.cloud.spanner.spi.v1.SpannerRpc;
@@ -39,7 +36,6 @@ import com.google.spanner.v1.ExecuteBatchDmlRequest;
 import com.google.spanner.v1.ExecuteBatchDmlResponse;
 import com.google.spanner.v1.ResultSet;
 import com.google.spanner.v1.ResultSetStats;
-import io.grpc.Context;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import java.util.Arrays;
@@ -57,7 +53,6 @@ import org.mockito.MockitoAnnotations;
 public class TransactionRunnerImplTest {
   @Mock private SpannerRpc rpc;
   @Mock private SessionImpl session;
-  @Mock private TransactionRunnerImpl.Sleeper sleeper;
   @Mock private TransactionRunnerImpl.TransactionContextImpl txn;
   private TransactionRunnerImpl transactionRunner;
   private boolean firstRun;
@@ -67,7 +62,7 @@ public class TransactionRunnerImplTest {
     MockitoAnnotations.initMocks(this);
     firstRun = true;
     when(session.newTransaction()).thenReturn(txn);
-    transactionRunner = new TransactionRunnerImpl(session, rpc, sleeper, 1);
+    transactionRunner = new TransactionRunnerImpl(session, rpc, 1);
   }
 
   @Test
@@ -89,10 +84,8 @@ public class TransactionRunnerImplTest {
   @Test
   public void runAbort() {
     when(txn.isAborted()).thenReturn(true);
-    long backoffMillis = 100L;
-    when(txn.getRetryDelayInMillis(any(BackOff.class))).thenReturn(backoffMillis);
     runTransaction(SpannerExceptionFactory.newSpannerException(ErrorCode.ABORTED, ""));
-    verify(sleeper, times(1)).backoffSleep(any(Context.class), eq(backoffMillis));
+    verify(txn, times(2)).ensureTxn();
   }
 
   @Test
@@ -101,8 +94,6 @@ public class TransactionRunnerImplTest {
         SpannerExceptionFactory.newSpannerException(
             SpannerExceptionFactory.newSpannerException(ErrorCode.ABORTED, ""));
     doThrow(error).doNothing().when(txn).commit();
-    long backoffMillis = 100L;
-    when(txn.getRetryDelayInMillis(any(BackOff.class))).thenReturn(backoffMillis);
     final AtomicInteger numCalls = new AtomicInteger(0);
     transactionRunner.run(
         new TransactionCallable<Void>() {
@@ -113,7 +104,6 @@ public class TransactionRunnerImplTest {
           }
         });
     assertThat(numCalls.get()).isEqualTo(2);
-    verify(sleeper, times(1)).backoffSleep(any(Context.class), eq(backoffMillis));
     verify(txn, times(2)).ensureTxn();
   }
 

google-cloud-spanner/src/main/java/com/google/cloud/spanner/SpannerImpl.java
@@ -245,16 +245,6 @@ class SpannerImpl extends BaseService<SpannerOptions> implements Spanner {
   }
 
   /**
-   * Checks that the current context is still valid, throwing a CANCELLED or DEADLINE_EXCEEDED error
-   * if not.
-   */
-  static void checkContext(Context context) {
-    if (context.isCancelled()) {
-      throw newSpannerExceptionForCancellation(context, null);
-    }
-  }
-
-  /**
    * Encapsulates state to be passed to the {@link SpannerRpc} layer for a given session. Currently
    * used to select the {@link io.grpc.Channel} to be used in issuing the RPCs in a Session.
    */

commit 04ebd9ccec7ba071c84922cef031552be321b3f8

TransactionRunner did not use prepared transaction (#6275)
A Spanner transaction runner that was given a session with a prepared
read/write transaction would not use that transaction, but instead
always start a new one, causing an extra unnecessary round-trip to the
server. This regression was introduced by commit
b3c50edec74efa7891e6f1e8e762996c79bee75a (PR #5433).

google-cloud-spanner/src/main/java/com/google/cloud/spanner/TransactionRunnerImpl.java
@@ -80,7 +80,7 @@ class TransactionRunnerImpl implements SessionTransaction, TransactionRunner {
     }
 
     void ensureTxn() {
-      if (transactionId == null) {
+      if (transactionId == null || isAborted()) {
         span.addAnnotation("Creating Transaction");
         try {
           transactionId = session.beginTransaction();
@@ -134,6 +134,9 @@ class TransactionRunnerImpl implements SessionTransaction, TransactionRunner {
       } catch (RuntimeException e) {
         span.addAnnotation("Commit Failed", TraceUtil.getExceptionAnnotations(e));
         TraceUtil.endSpanWithFailure(opSpan, e);
+        if (e instanceof SpannerException) {
+          onError((SpannerException) e);
+        }
         throw e;
       }
       span.addAnnotation("Commit Done");
@@ -291,6 +294,7 @@ class TransactionRunnerImpl implements SessionTransaction, TransactionRunner {
   TransactionRunnerImpl(SessionImpl session, SpannerRpc rpc, int defaultPrefetchChunks) {
     this.session = session;
     this.span = Tracing.getTracer().getCurrentSpan();
+    this.txn = session.newTransaction();
   }
 
   @Nullable
@@ -318,7 +322,9 @@ class TransactionRunnerImpl implements SessionTransaction, TransactionRunner {
         new Callable<T>() {
           @Override
           public T call() {
-            txn = session.newTransaction();
+            if (txn.isAborted()) {
+              txn = session.newTransaction();
+            }
             checkState(
                 isValid,
                 "TransactionRunner has been invalidated by a new operation on the session");

google-cloud-spanner/src/test/java/com/google/cloud/spanner/TransactionManagerImplTest.java
@@ -19,12 +19,25 @@ package com.google.cloud.spanner;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import com.google.cloud.Timestamp;
+import com.google.cloud.grpc.GrpcTransportOptions;
+import com.google.cloud.grpc.GrpcTransportOptions.ExecutorFactory;
 import com.google.cloud.spanner.TransactionManager.TransactionState;
+import com.google.cloud.spanner.spi.v1.SpannerRpc;
+import com.google.protobuf.ByteString;
+import com.google.spanner.v1.BeginTransactionRequest;
+import com.google.spanner.v1.CommitRequest;
+import com.google.spanner.v1.CommitResponse;
+import com.google.spanner.v1.Transaction;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,9 +46,23 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 @RunWith(JUnit4.class)
 public class TransactionManagerImplTest {
+  private static final class TestExecutorFactory
+      implements ExecutorFactory<ScheduledExecutorService> {
+    @Override
+    public ScheduledExecutorService get() {
+      return Executors.newSingleThreadScheduledExecutor();
+    }
+
+    @Override
+    public void release(ScheduledExecutorService exec) {
+      exec.shutdown();
+    }
+  }
 
   @Rule public ExpectedException exception = ExpectedException.none();
 
@@ -153,5 +180,64 @@ public class TransactionManagerImplTest {
     manager.rollback();
     exception.expect(IllegalStateException.class);
     manager.commit();
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void usesPreparedTransaction() {
+    SpannerOptions options = mock(SpannerOptions.class);
+    GrpcTransportOptions transportOptions = mock(GrpcTransportOptions.class);
+    when(transportOptions.getExecutorFactory()).thenReturn(new TestExecutorFactory());
+    when(options.getTransportOptions()).thenReturn(transportOptions);
+    SessionPoolOptions sessionPoolOptions =
+        SessionPoolOptions.newBuilder().setMinSessions(0).build();
+    when(options.getSessionPoolOptions()).thenReturn(sessionPoolOptions);
+    SpannerRpc rpc = mock(SpannerRpc.class);
+    when(rpc.createSession(Mockito.anyString(), Mockito.anyMap(), Mockito.anyMap()))
+        .thenAnswer(
+            new Answer<com.google.spanner.v1.Session>() {
+              @Override
+              public com.google.spanner.v1.Session answer(InvocationOnMock invocation)
+                  throws Throwable {
+                return com.google.spanner.v1.Session.newBuilder()
+                    .setName((String) invocation.getArguments()[0])
+                    .setCreateTime(
+                        com.google.protobuf.Timestamp.newBuilder()
+                            .setSeconds(System.currentTimeMillis() * 1000))
+                    .build();
+              }
+            });
+    when(rpc.beginTransaction(Mockito.any(BeginTransactionRequest.class), Mockito.anyMap()))
+        .thenAnswer(
+            new Answer<Transaction>() {
+              @Override
+              public Transaction answer(InvocationOnMock invocation) throws Throwable {
+                return Transaction.newBuilder()
+                    .setId(ByteString.copyFromUtf8(UUID.randomUUID().toString()))
+                    .build();
+              }
+            });
+    when(rpc.commit(Mockito.any(CommitRequest.class), Mockito.anyMap()))
+        .thenAnswer(
+            new Answer<CommitResponse>() {
+              @Override
+              public CommitResponse answer(InvocationOnMock invocation) throws Throwable {
+                return CommitResponse.newBuilder()
+                    .setCommitTimestamp(
+                        com.google.protobuf.Timestamp.newBuilder()
+                            .setSeconds(System.currentTimeMillis() * 1000))
+                    .build();
+              }
+            });
+    DatabaseId db = DatabaseId.of("test", "test", "test");
+    try (SpannerImpl spanner = new SpannerImpl(rpc, options)) {
+      DatabaseClient client = spanner.getDatabaseClient(db);
+      try (TransactionManager mgr = client.transactionManager()) {
+        mgr.begin();
+        mgr.commit();
+      }
+      verify(rpc, times(1))
+          .beginTransaction(Mockito.any(BeginTransactionRequest.class), Mockito.anyMap());
+    }
   }
 }

google-cloud-spanner/src/test/java/com/google/cloud/spanner/TransactionRunnerImplTest.java
@@ -19,10 +19,13 @@ package com.google.cloud.spanner;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.google.cloud.grpc.GrpcTransportOptions;
+import com.google.cloud.grpc.GrpcTransportOptions.ExecutorFactory;
 import com.google.cloud.spanner.TransactionRunner.TransactionCallable;
 import com.google.cloud.spanner.TransactionRunnerImpl.TransactionContextImpl;
 import com.google.cloud.spanner.spi.v1.SpannerRpc;
@@ -30,15 +33,20 @@ import com.google.common.base.Preconditions;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Timestamp;
 import com.google.rpc.Code;
+import com.google.spanner.v1.BeginTransactionRequest;
 import com.google.spanner.v1.CommitRequest;
 import com.google.spanner.v1.CommitResponse;
 import com.google.spanner.v1.ExecuteBatchDmlRequest;
 import com.google.spanner.v1.ExecuteBatchDmlResponse;
 import com.google.spanner.v1.ResultSet;
 import com.google.spanner.v1.ResultSetStats;
+import com.google.spanner.v1.Transaction;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import java.util.Arrays;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.Before;
 import org.junit.Test;
@@ -47,10 +55,25 @@ import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 /** Unit test for {@link com.google.cloud.spanner.SpannerImpl.TransactionRunnerImpl} */
 @RunWith(JUnit4.class)
 public class TransactionRunnerImplTest {
+  private static final class TestExecutorFactory
+      implements ExecutorFactory<ScheduledExecutorService> {
+    @Override
+    public ScheduledExecutorService get() {
+      return Executors.newSingleThreadScheduledExecutor();
+    }
+
+    @Override
+    public void release(ScheduledExecutorService exec) {
+      exec.shutdown();
+    }
+  }
+
   @Mock private SpannerRpc rpc;
   @Mock private SessionImpl session;
   @Mock private TransactionRunnerImpl.TransactionContextImpl txn;
@@ -63,6 +86,68 @@ public class TransactionRunnerImplTest {
     firstRun = true;
     when(session.newTransaction()).thenReturn(txn);
     transactionRunner = new TransactionRunnerImpl(session, rpc, 1);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void usesPreparedTransaction() {
+    SpannerOptions options = mock(SpannerOptions.class);
+    GrpcTransportOptions transportOptions = mock(GrpcTransportOptions.class);
+    when(transportOptions.getExecutorFactory()).thenReturn(new TestExecutorFactory());
+    when(options.getTransportOptions()).thenReturn(transportOptions);
+    SessionPoolOptions sessionPoolOptions =
+        SessionPoolOptions.newBuilder().setMinSessions(0).build();
+    when(options.getSessionPoolOptions()).thenReturn(sessionPoolOptions);
+    SpannerRpc rpc = mock(SpannerRpc.class);
+    when(rpc.createSession(Mockito.anyString(), Mockito.anyMap(), Mockito.anyMap()))
+        .thenAnswer(
+            new Answer<com.google.spanner.v1.Session>() {
+              @Override
+              public com.google.spanner.v1.Session answer(InvocationOnMock invocation)
+                  throws Throwable {
+                return com.google.spanner.v1.Session.newBuilder()
+                    .setName((String) invocation.getArguments()[0])
+                    .setCreateTime(
+                        Timestamp.newBuilder().setSeconds(System.currentTimeMillis() * 1000))
+                    .build();
+              }
+            });
+    when(rpc.beginTransaction(Mockito.any(BeginTransactionRequest.class), Mockito.anyMap()))
+        .thenAnswer(
+            new Answer<Transaction>() {
+              @Override
+              public Transaction answer(InvocationOnMock invocation) throws Throwable {
+                return Transaction.newBuilder()
+                    .setId(ByteString.copyFromUtf8(UUID.randomUUID().toString()))
+                    .build();
+              }
+            });
+    when(rpc.commit(Mockito.any(CommitRequest.class), Mockito.anyMap()))
+        .thenAnswer(
+            new Answer<CommitResponse>() {
+              @Override
+              public CommitResponse answer(InvocationOnMock invocation) throws Throwable {
+                return CommitResponse.newBuilder()
+                    .setCommitTimestamp(
+                        Timestamp.newBuilder().setSeconds(System.currentTimeMillis() * 1000))
+                    .build();
+              }
+            });
+    DatabaseId db = DatabaseId.of("test", "test", "test");
+    try (SpannerImpl spanner = new SpannerImpl(rpc, options)) {
+      DatabaseClient client = spanner.getDatabaseClient(db);
+      client
+          .readWriteTransaction()
+          .run(
+              new TransactionCallable<Void>() {
+                @Override
+                public Void run(TransactionContext transaction) throws Exception {
+                  return null;
+                }
+              });
+      verify(rpc, times(1))
+          .beginTransaction(Mockito.any(BeginTransactionRequest.class), Mockito.anyMap());
+    }
   }
 
   @Test
@@ -168,8 +253,11 @@ public class TransactionRunnerImplTest {
   private long[] batchDmlException(int status) {
     Preconditions.checkArgument(status != Code.OK_VALUE);
     TransactionContextImpl transaction =
-        new TransactionContextImpl(session, ByteString.copyFromUtf8("test"), rpc, 10);
+        new TransactionContextImpl(
+            session, ByteString.copyFromUtf8(UUID.randomUUID().toString()), rpc, 10);
     when(session.newTransaction()).thenReturn(transaction);
+    when(session.beginTransaction())
+        .thenReturn(ByteString.copyFromUtf8(UUID.randomUUID().toString()));
     when(session.getName()).thenReturn("test");
     TransactionRunnerImpl runner = new TransactionRunnerImpl(session, rpc, 10);
     ExecuteBatchDmlResponse response1 =