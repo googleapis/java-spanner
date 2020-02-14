67058f13b1ef30db42cc1ecf6008aefb4783bf6d
Mayur Kale <mayurkale@google.com>
2020-02-13 19:24:16 -0800

    rename get_sessions_timeouts to get_session_timeouts

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/MetricRegistryConstants.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/MetricRegistryConstants.java
@@ -45,7 +45,7 @@ class MetricRegistryConstants {
   static final String MAX_IN_USE_SESSIONS = "cloud.google.com/java/spanner/max_in_use_sessions";
   static final String MAX_ALLOWED_SESSIONS = "cloud.google.com/java/spanner/max_allowed_sessions";
   static final String IN_USE_SESSIONS = "cloud.google.com/java/spanner/in_use_sessions";
-  static final String GET_SESSIONS_TIMEOUTS = "cloud.google.com/java/spanner/get_sessions_timeouts";
+  static final String GET_SESSION_TIMEOUTS = "cloud.google.com/java/spanner/get_session_timeouts";
 
   static final String MAX_IN_USE_SESSIONS_DESCRIPTION =
       "The maximum number of sessions in use during the last 10 minute interval.";
