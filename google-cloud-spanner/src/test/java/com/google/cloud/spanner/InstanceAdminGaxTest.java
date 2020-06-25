8536a3e05252bfbae6872fc011468ce353f57f2c
Knut Olav Løite <koloite@gmail.com>
2020-06-25 12:18:21 +0200

    tests: fix flaky instance admin test (#303)

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/InstanceAdminGaxTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/InstanceAdminGaxTest.java
@@ -229,7 +229,7 @@ public class InstanceAdminGaxTest {
             .setMaxRpcTimeout(Duration.ofMillis(200L))
             .setRetryDelayMultiplier(1000.0d)
             .setMaxAttempts(10)
-            .setTotalTimeout(Duration.ofMillis(200L))
+            .setTotalTimeout(Duration.ofMillis(20000L))
             .build();
     RetrySettings retrySettingsWithHighTimeout =
         RetrySettings.newBuilder()
