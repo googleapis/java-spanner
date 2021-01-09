cd744deea87d0a59f29f7031d2e7e840b05e5dff
yoshi-automation <yoshi-automation@google.com>
2021-01-09 14:30:06 -0800

    chore: update gapic-generator-java to 0.0.18
    Committer: @miraleung
    PiperOrigin-RevId: 350949863
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Sat Jan 9 14:01:49 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 91e206bcfeaf8948ea03fe3cb1b7616108496cd3
    Source-Link: https://github.com/googleapis/googleapis/commit/91e206bcfeaf8948ea03fe3cb1b7616108496cd3

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/admin/instance/v1/InstanceAdminClient.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/admin/instance/v1/InstanceAdminClient.java
@@ -131,8 +131,7 @@ import javax.annotation.Generated;
  *
  * <p>Please refer to the GitHub repository's samples for more quickstart code snippets.
  */
-@BetaApi
-@Generated("by gapic-generator")
+@Generated("by gapic-generator-java")
 public class InstanceAdminClient implements BackgroundResource {
   private final InstanceAdminSettings settings;
   private final InstanceAdminStub stub;
