0a5f1cc4c89062688d527c1827506c80ee1f3e7c
Thiago Nunes <thiagotnunes@google.com>
2021-02-09 14:17:12 +1100

    test: enhaces the tests of numeric as primary key

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITDatabaseTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITDatabaseTest.java
@@ -17,6 +17,8 @@
 package com.google.cloud.spanner.it;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 import com.google.api.client.util.ExponentialBackOff;
@@ -30,15 +32,22 @@ import com.google.cloud.spanner.ErrorCode;
 import com.google.cloud.spanner.InstanceId;
 import com.google.cloud.spanner.InstanceNotFoundException;
 import com.google.cloud.spanner.IntegrationTestEnv;
+import com.google.cloud.spanner.KeySet;
+import com.google.cloud.spanner.Mutation;
 import com.google.cloud.spanner.ParallelIntegrationTest;
 import com.google.cloud.spanner.ResultSet;
 import com.google.cloud.spanner.SpannerException;
 import com.google.cloud.spanner.Statement;
+import com.google.cloud.spanner.TransactionContext;
+import com.google.cloud.spanner.TransactionRunner.TransactionCallable;
 import com.google.spanner.admin.database.v1.CreateDatabaseMetadata;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import javax.annotation.Nullable;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -145,22 +154,65 @@ public class ITDatabaseTest {
   }
 
   @Test
-  public void numericPrimaryKey()
+  public void testNumericPrimaryKey()
       throws InterruptedException, ExecutionException, TimeoutException {
+    final String projectId = env.getTestHelper().getInstanceId().getProject();
     final String instanceId = env.getTestHelper().getInstanceId().getInstance();
     final String databaseId = env.getTestHelper().getUniqueDatabaseId();
+    final String table = "NumericTable";
+    final DatabaseId id = DatabaseId.of(projectId, instanceId, databaseId);
     final DatabaseAdminClient databaseAdminClient =
         env.getTestHelper().getClient().getDatabaseAdminClient();
 
     try {
-      final OperationFuture<Database, CreateDatabaseMetadata> op =
+      // Creates table with numeric primary key
+      final OperationFuture<Database, CreateDatabaseMetadata> operation =
           databaseAdminClient.createDatabase(
               instanceId,
               databaseId,
               Collections.singletonList(
-                  "CREATE TABLE NumericTable (" + "Id NUMERIC NOT NULL" + ") PRIMARY KEY (Id)"));
-      final Database database = op.get(1, TimeUnit.MINUTES);
-      assertThat(database).isNotNull();
+                  "CREATE TABLE " + table + " (" + "Id NUMERIC NOT NULL" + ") PRIMARY KEY (Id)"));
+      final Database database = operation.get(10, TimeUnit.MINUTES);
+      assertNotNull(database);
+
+      // Writes data into the table
+      final DatabaseClient databaseClient = env.getTestHelper().getClient().getDatabaseClient(id);
+      final ArrayList<Mutation> mutations = new ArrayList<>();
+      for (int i = 0; i < 5; i++) {
+        mutations.add(
+            Mutation.newInsertBuilder(table).set("Id").to(new BigDecimal(i + "")).build());
+      }
+      databaseClient.write(mutations);
+
+      // Reads the data to verify the writes
+      try (final ResultSet resultSet =
+          databaseClient.singleUse().read(table, KeySet.all(), Collections.singletonList("Id"))) {
+        for (int i = 0; resultSet.next(); i++) {
+          assertEquals(new BigDecimal(i + ""), resultSet.getBigDecimal("Id"));
+        }
+      }
+
+      // Deletes data from the table, leaving only the Id = 0 row
+      databaseClient
+          .readWriteTransaction()
+          .run(
+              new TransactionCallable<Object>() {
+                @Nullable
+                @Override
+                public Object run(TransactionContext transaction) throws Exception {
+                  transaction.executeUpdate(Statement.of("DELETE FROM " + table + " WHERE Id > 0"));
+                  return null;
+                }
+              });
+
+      // Reads the data to verify the deletes only left a single row left
+      try (final ResultSet resultSet =
+          databaseClient
+              .singleUse()
+              .executeQuery(Statement.of("SELECT COUNT(1) as cnt FROM " + table))) {
+        resultSet.next();
+        assertEquals(1L, resultSet.getLong("cnt"));
+      }
     } finally {
       databaseAdminClient.dropDatabase(instanceId, databaseId);
     }
