255e66cc8e8e60150722a58a9f579c0362f83ae0
Thiago Nunes <thiagotnunes@google.com>
2021-07-06 09:04:01 +1000

    test: add it test for instance leader options

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITInstanceAdminTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITInstanceAdminTest.java
@@ -57,9 +57,11 @@ public class ITInstanceAdminTest {
     List<InstanceConfig> configs = new ArrayList<>();
     Iterators.addAll(configs, instanceClient.listInstanceConfigs().iterateAll().iterator());
     assertThat(configs.isEmpty()).isFalse();
+    configs.forEach(config -> assertThat(config.getLeaderOptions()).isNotEmpty());
     InstanceConfig config =
         instanceClient.getInstanceConfig(configs.get(0).getId().getInstanceConfig());
     assertThat(config.getId()).isEqualTo(configs.get(0).getId());
+    assertThat(config.getLeaderOptions()).isNotEmpty();
     config = config.reload();
     assertThat(config.getId()).isEqualTo(configs.get(0).getId());
   }
