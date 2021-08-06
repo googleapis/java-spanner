37b5fe0e3b77f60f94e9d10ca377e563786545bc
yoshi-automation <yoshi-automation@google.com>
2021-08-06 01:46:56 -0700

    chore: release gapic-generator-java v1.0.17
    Committer: @miraleung
    PiperOrigin-RevId: 388499329
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 3 11:12:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bb0a090d9204110042ab5dee2ce9e06e8071ce54
    Source-Link: https://github.com/googleapis/googleapis/commit/bb0a090d9204110042ab5dee2ce9e06e8071ce54

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/admin/database/v1/stub/GrpcDatabaseAdminStub.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/admin/database/v1/stub/GrpcDatabaseAdminStub.java
@@ -794,7 +794,13 @@ public class GrpcDatabaseAdminStub extends DatabaseAdminStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
