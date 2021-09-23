cada56263a1f8a10bded5151c1e0e44ad633c0df
yoshi-automation <yoshi-automation@google.com>
2021-09-23 14:59:37 -0700

    chore: update gapic-generator-java version to 2.1.0
    PiperOrigin-RevId: 391407209
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 17 16:48:10 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: e1738ee972b49fd0c4d4718b035444495142b781
    Source-Link: https://github.com/googleapis/googleapis/commit/e1738ee972b49fd0c4d4718b035444495142b781

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/admin/instance/v1/stub/InstanceAdminStubSettings.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/admin/instance/v1/stub/InstanceAdminStubSettings.java
@@ -350,7 +350,9 @@ public class InstanceAdminStubSettings extends StubSettings<InstanceAdminStubSet
 
   /** Returns a builder for the default credentials for this service. */
   public static GoogleCredentialsProvider.Builder defaultCredentialsProviderBuilder() {
-    return GoogleCredentialsProvider.newBuilder().setScopesToApply(DEFAULT_SERVICE_SCOPES);
+    return GoogleCredentialsProvider.newBuilder()
+        .setScopesToApply(DEFAULT_SERVICE_SCOPES)
+        .setUseJwtAccessWithScope(true);
   }
 
   /** Returns a builder for the default ChannelProvider for this service. */
