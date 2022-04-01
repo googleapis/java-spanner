4c244dccaa454581f9110006120df572fe43ee21
Rajat Bhatta <93644539+rajatbhatta@users.noreply.github.com>
2022-04-01 16:58:45 +0530

    Update google-cloud-spanner/src/main/java/com/google/cloud/spanner/InstanceConfigInfo.java
    Co-authored-by: Knut Olav Løite <koloite@gmail.com>

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/InstanceConfigInfo.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/InstanceConfigInfo.java
@@ -118,7 +118,7 @@ public class InstanceConfigInfo {
 
   /**
    * Base configuration, e.g. projects/<project_name>/instanceConfigs/nam3, based on which this
-   * configuration is created. Only set for user managed configurations. base_config must refer to a
+   * configuration is created. Only set for user managed configurations. The base config must refer to a
    * configuration of type GOOGLE_MANAGED.
    */
   public InstanceConfigInfo getBaseConfig() {
