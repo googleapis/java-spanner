0b2191b9bcec89f64bfccfa802126f6a826d17a8
Astha Mohta <35952883+asthamohta@users.noreply.github.com>
2022-04-05 22:57:46 +0530

    Copy backup samples (#1806)
    * Revert "removing samples"
    
    This reverts commit b06a3df559ee901a84a14a99624fa985b781d52d.
    
    * removing samples
    
    * changes to samples
    
    * linting
    
    * Update samples/snippets/src/main/java/com/example/spanner/SpannerSample.java
    
    Co-authored-by: Knut Olav Løite <koloite@gmail.com>
    
    * Update samples/snippets/src/main/java/com/example/spanner/CopyBackupSample.java
    
    Co-authored-by: Knut Olav Løite <koloite@gmail.com>
    
    * changes as per comments
    
    Co-authored-by: Knut Olav Løite <koloite@gmail.com>

--- samples/snippets/src/test/java/com/example/spanner/SpannerSampleIT.java
+++ samples/snippets/src/test/java/com/example/spanner/SpannerSampleIT.java
@@ -17,6 +17,7 @@
 package com.example.spanner;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import com.google.cloud.Timestamp;
 import com.google.cloud.spanner.Backup;
@@ -95,7 +96,7 @@ public class SpannerSampleIT {
     key = String.format("projects/%s/locations/%s/keyRings/%s/cryptoKeys/%s",
         options.getProjectId(), keyLocation, keyRing, keyName);
   }
-  
+
   static void deleteStaleTestDatabases(String instanceId, String baseDbId) {
     Timestamp now = Timestamp.now();
     Pattern samplePattern = getTestDbIdPattern(baseDbId);
@@ -328,6 +329,8 @@ public class SpannerSampleIT {
               "Backup %s on database %s pending:",
               backupId.getName(),
               dbId.getName()));
+      assertTrue("Out does not contain copy backup operations", out.contains(
+              "Copy Backup Operations"));
     } catch (SpannerException e) {
       assertThat(e.getErrorCode()).isEqualTo(ErrorCode.INVALID_ARGUMENT);
       assertThat(e.getMessage()).contains("Cannot evaluate filter expression");
@@ -407,7 +410,7 @@ public class SpannerSampleIT {
     out = runSample("deletebackup");
     assertThat(out).contains("Deleted backup [" + backupId + "]");
   }
-  
+
   @Test
   public void testEncryptedDatabaseAndBackupSamples() throws Exception {
     String projectId = spanner.getOptions().getProjectId();
@@ -510,7 +513,7 @@ public class SpannerSampleIT {
   private static int countOccurrences(String input, String search) {
     return input.split(search).length - 1;
   }
-  
+
   private static String toComparableId(String baseId, String existingId) {
     String zeroUuid = "00000000-0000-0000-0000-0000-00000000";
     int shouldBeLength = (baseId + "-" + zeroUuid).length();
@@ -523,7 +526,7 @@ public class SpannerSampleIT {
         baseDbId + "-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{8}",
         Pattern.CASE_INSENSITIVE);
   }
-  
+
   static String formatForTest(String name) {
     return name + "-" + UUID.randomUUID().toString().substring(0, DBID_LENGTH);
   }
