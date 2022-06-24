967d3d033f1bfc1b3224d0497983aad78d869bb3
Robert Wang <rbrtwng.cn@gmail.com>
2022-06-24 08:06:12 -0700

    Remove the statement of session number limits
    Cloud Spanner no longer has the 10K "Sessions per database per node" limit.

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/Session.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/Session.java
@@ -28,10 +28,10 @@ import com.google.protobuf.Empty;
  * read-write/write-only transactions, create multiple sessions. Note that standalone reads and
  * queries use a transaction internally, and count toward the one transaction limit.
  *
- * <p>Cloud Spanner limits the number of sessions that can exist at any given time; thus, it is a
- * good idea to delete idle and/or unneeded sessions. Aside from explicit deletes, Cloud Spanner can
- * delete sessions for which no operations are sent for more than an hour, or due to internal
- * errors. If a session is deleted, requests to it return {@link ErrorCode#NOT_FOUND}.
+ * <p>It is a good idea to delete idle and/or unneeded sessions. Aside from explicit deletes,
+ * Cloud Spanner can delete sessions for which no operations are sent for more than an hour,
+ * or due to internal errors. If a session is deleted, requests to it return
+ * {@link ErrorCode#NOT_FOUND}. 
  *
  * <p>Idle sessions can be kept alive by sending a trivial SQL query periodically, for example,
  * {@code SELECT 1}.
