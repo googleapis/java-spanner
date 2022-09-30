22ed9997ff29de3f60c67138b46ace6e58570328
Rajat Bhatta <rajatrb@google.com>
2022-09-30 03:58:05 +0000

    fix: linting

--- samples/snippets/src/main/java/com/example/spanner/ListInstanceConfigOperationsSample.java
+++ samples/snippets/src/main/java/com/example/spanner/ListInstanceConfigOperationsSample.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.spanner;
 
 // [START spanner_list_instance_config_operations]
