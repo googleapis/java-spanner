84d660c870f3fbece05392fe0dd5670f0ae422a0
gauravpurohit <gauravpurohit@google.com>
2023-02-24 14:51:50 +0000

    style: lint

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITDatabaseRolePermissionTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITDatabaseRolePermissionTest.java
@@ -100,8 +100,8 @@ public class ITDatabaseRolePermissionTest {
             .get(5, TimeUnit.MINUTES);
 
     // Connect to db with dbRoleParent.
-    SpannerOptions options = testHelper.getOptions().toBuilder().setDatabaseRole(dbRoleParent)
-        .build();
+    SpannerOptions options =
+        testHelper.getOptions().toBuilder().setDatabaseRole(dbRoleParent).build();
 
     Spanner spanner = options.getService();
     DatabaseClient dbClient = spanner.getDatabaseClient(createdDatabase.getId());
