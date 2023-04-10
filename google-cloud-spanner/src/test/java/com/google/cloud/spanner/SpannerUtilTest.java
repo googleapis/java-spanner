2222367f5918f0a8096bd030c1b83c95ceb46989
rajatrb <rajatrb@google.com>
2023-04-10 14:21:00 +0000

    feat: add support for Directed Read options

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/SpannerUtilTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/SpannerUtilTest.java
@@ -17,35 +17,42 @@ import org.junit.runners.JUnit4;
 /** Unit tests for {@link com.google.cloud.spanner.SpannerUtil}. */
 @RunWith(JUnit4.class)
 public class SpannerUtilTest {
-  private DirectedReadOptions getDirectedReadOptions_IncludeReplica_ReplicaSelectionCountGreaterThanMax() {
-    List<ReplicaSelection> replicaSelectionList = new ArrayList<>(
-        Collections.nCopies(SpannerUtil.MAX_REPLICA_SELECTIONS_COUNT + 1,
-            ReplicaSelection.newBuilder().setLocation("us-west1").build()));
-    return
-        DirectedReadOptions
-            .newBuilder()
-            .setIncludeReplicas(IncludeReplicas.newBuilder().addAllReplicaSelections(replicaSelectionList))
-            .build();
+  private DirectedReadOptions
+      getDirectedReadOptions_IncludeReplica_ReplicaSelectionCountGreaterThanMax() {
+    List<ReplicaSelection> replicaSelectionList =
+        new ArrayList<>(
+            Collections.nCopies(
+                SpannerUtil.MAX_REPLICA_SELECTIONS_COUNT + 1,
+                ReplicaSelection.newBuilder().setLocation("us-west1").build()));
+    return DirectedReadOptions.newBuilder()
+        .setIncludeReplicas(
+            IncludeReplicas.newBuilder().addAllReplicaSelections(replicaSelectionList))
+        .build();
   }
 
-  private DirectedReadOptions getDirectedReadOptions_ExcludeReplica_ReplicaSelectionCountGreaterThanMax() {
-    List<ReplicaSelection> replicaSelectionList = new ArrayList<>(
-        Collections.nCopies(SpannerUtil.MAX_REPLICA_SELECTIONS_COUNT + 1,
-            ReplicaSelection.newBuilder().setLocation("us-east1").build()));
-    return
-        DirectedReadOptions
-            .newBuilder()
-            .setExcludeReplicas(ExcludeReplicas.newBuilder().addAllReplicaSelections(replicaSelectionList))
-            .build();
+  private DirectedReadOptions
+      getDirectedReadOptions_ExcludeReplica_ReplicaSelectionCountGreaterThanMax() {
+    List<ReplicaSelection> replicaSelectionList =
+        new ArrayList<>(
+            Collections.nCopies(
+                SpannerUtil.MAX_REPLICA_SELECTIONS_COUNT + 1,
+                ReplicaSelection.newBuilder().setLocation("us-east1").build()));
+    return DirectedReadOptions.newBuilder()
+        .setExcludeReplicas(
+            ExcludeReplicas.newBuilder().addAllReplicaSelections(replicaSelectionList))
+        .build();
   }
 
   public DirectedReadOptions getDirectedReadOptions_BothIncludeExcludeReplicasSet() {
-    return DirectedReadOptions
-        .newBuilder()
-        .setExcludeReplicas(ExcludeReplicas.newBuilder().addReplicaSelections(
-            ReplicaSelection.newBuilder().setLocation("us-east1").build()).build())
-        .setIncludeReplicas(IncludeReplicas.newBuilder().addReplicaSelections(
-            ReplicaSelection.newBuilder().setLocation("us-west1").build()).build())
+    return DirectedReadOptions.newBuilder()
+        .setExcludeReplicas(
+            ExcludeReplicas.newBuilder()
+                .addReplicaSelections(ReplicaSelection.newBuilder().setLocation("us-east1").build())
+                .build())
+        .setIncludeReplicas(
+            IncludeReplicas.newBuilder()
+                .addReplicaSelections(ReplicaSelection.newBuilder().setLocation("us-west1").build())
+                .build())
         .build();
   }
 
@@ -54,23 +61,34 @@ public class SpannerUtilTest {
     // This test can be skipped because using the proto object directly will handle this case
     // automatically and will set only IncludeReplicas, if both IncludeReplicas and ExcludeReplicas
     // are passed in.
-    DirectedReadOptions directedReadOptions = getDirectedReadOptions_BothIncludeExcludeReplicasSet();
-    SpannerException e = assertThrows(SpannerException.class, () -> SpannerUtil.verifyDirectedReadOptions(directedReadOptions));
+    DirectedReadOptions directedReadOptions =
+        getDirectedReadOptions_BothIncludeExcludeReplicasSet();
+    SpannerException e =
+        assertThrows(
+            SpannerException.class,
+            () -> SpannerUtil.verifyDirectedReadOptions(directedReadOptions));
     assertEquals(e.getErrorCode(), ErrorCode.INVALID_ARGUMENT);
   }
 
   @Test
   public void testDirectedReadOptions_IncludeReplica_ReplicaSelectionCountGreaterThanMax() {
-    DirectedReadOptions directedReadOptions = getDirectedReadOptions_IncludeReplica_ReplicaSelectionCountGreaterThanMax();
-    SpannerException e = assertThrows(SpannerException.class, () -> SpannerUtil.verifyDirectedReadOptions(directedReadOptions));
+    DirectedReadOptions directedReadOptions =
+        getDirectedReadOptions_IncludeReplica_ReplicaSelectionCountGreaterThanMax();
+    SpannerException e =
+        assertThrows(
+            SpannerException.class,
+            () -> SpannerUtil.verifyDirectedReadOptions(directedReadOptions));
     assertEquals(e.getErrorCode(), ErrorCode.INVALID_ARGUMENT);
   }
 
   @Test
   public void testDirectedReadOptions_ExcludeReplica_ReplicaSelectionCountGreaterThanMax() {
-    DirectedReadOptions directedReadOptions = getDirectedReadOptions_ExcludeReplica_ReplicaSelectionCountGreaterThanMax();
-    SpannerException e = assertThrows(SpannerException.class, () -> SpannerUtil.verifyDirectedReadOptions(directedReadOptions));
+    DirectedReadOptions directedReadOptions =
+        getDirectedReadOptions_ExcludeReplica_ReplicaSelectionCountGreaterThanMax();
+    SpannerException e =
+        assertThrows(
+            SpannerException.class,
+            () -> SpannerUtil.verifyDirectedReadOptions(directedReadOptions));
     assertEquals(e.getErrorCode(), ErrorCode.INVALID_ARGUMENT);
   }
-
 }
