162a4987eea82d47dcfd1fe7172cf8cae7718c53
Rajat Bhatta <93644539+rajatbhatta@users.noreply.github.com>
2023-05-15 13:21:38 +0530

    Update snapshot.sh

--- .kokoro/release/snapshot.sh
+++ .kokoro/release/snapshot.sh
@@ -30,4 +30,5 @@ mvn clean deploy -B \
   -DperformRelease=true \
   -Dgpg.executable=gpg \
   -Dgpg.passphrase=${GPG_PASSPHRASE} \
-  -Dgpg.homedir=${GPG_HOMEDIR}
+  -Dgpg.homedir=${GPG_HOMEDIR} \
+  -Dclirr.skip=true
