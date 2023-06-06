44236647f1f748cff4406a7d1ea62eecae611c63
Arpan Mishra <arpanmishra@google.com>
2023-06-06 19:37:03 +0530

    Revert "Update google-cloud-spanner/src/main/java/com/google/cloud/spanner/SessionPoolOptions.java"
    This reverts commit b4b40334d88636bef7244f5f7773b500f447f101.

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/SessionPoolOptions.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/SessionPoolOptions.java
@@ -536,7 +536,7 @@ public class SessionPoolOptions {
      *
      * @return this builder for chaining
      */
-    public Builder setCloseLongRunningSessions() {
+    public Builder setCloseIfInactiveTransactions() {
       this.actionOnInactiveTransaction = ActionOnInactiveTransaction.CLOSE;
       return this;
     }
