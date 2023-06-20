e0353a2590690c853e85c8c66fe374aed79094c4
Gaurav Purohit <gauravpurohit@google.com>
2023-06-20 10:48:35 +0530

    feat: Adding support for databoost
    Enable databoost feature for all customers and removing the beta api annotation.

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/Options.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/Options.java
@@ -16,7 +16,6 @@
 
 package com.google.cloud.spanner;
 
-import com.google.api.core.BetaApi;
 import com.google.common.base.Preconditions;
 import com.google.spanner.v1.RequestOptions.Priority;
 import java.io.Serializable;
@@ -157,10 +156,8 @@ public final class Options implements Serializable {
 
   /**
    * If this is for a partitioned read & query and this field is set to `true`, the request will be
-   * executed via Spanner independent compute resources. The method is available in Beta mode (and
-   * is not generally available now).
+   * executed via Spanner independent compute resources.
    */
-  @BetaApi
   public static DataBoostQueryOption dataBoostEnabled(Boolean dataBoostEnabled) {
     return new DataBoostQueryOption(dataBoostEnabled);
   }
