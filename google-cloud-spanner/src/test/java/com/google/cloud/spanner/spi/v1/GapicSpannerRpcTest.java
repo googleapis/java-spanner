3b3c23592ff6475e82233ed7b81ba7070ace1fcd
Knut Olav Løite <koloite@gmail.com>
2023-10-13 16:15:31 +0200

    fix: always include default client lib header

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/spi/v1/GapicSpannerRpcTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/spi/v1/GapicSpannerRpcTest.java
@@ -33,6 +33,7 @@ import com.google.api.gax.rpc.ApiClientHeaderProvider;
 import com.google.api.gax.rpc.HeaderProvider;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.OAuth2Credentials;
+import com.google.cloud.ServiceOptions;
 import com.google.cloud.spanner.DatabaseClient;
 import com.google.cloud.spanner.DatabaseId;
 import com.google.cloud.spanner.Dialect;
@@ -77,6 +78,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
@@ -181,6 +183,12 @@ public class GapicSpannerRpcTest {
                     String auth =
                         headers.get(Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER));
                     assertThat(auth).isEqualTo("Bearer " + VARIABLE_OAUTH_TOKEN);
+                    String clientLibToken =
+                        headers.get(
+                            Metadata.Key.of("x-goog-api-client", Metadata.ASCII_STRING_MARSHALLER));
+                    assertNotNull(clientLibToken);
+                    assertTrue(
+                        clientLibToken.contains(ServiceOptions.getGoogApiClientLibName() + "/"));
                     if (call.getMethodDescriptor()
                             .equals(SpannerGrpc.getExecuteStreamingSqlMethod())
                         || call.getMethodDescriptor().equals(SpannerGrpc.getExecuteSqlMethod())) {
@@ -573,6 +581,27 @@ public class GapicSpannerRpcTest {
           });
     }
     assertFalse(isRouteToLeader);
+  }
+
+  @Test
+  public void testCustomClientLibToken_alsoContainsDefaultToken() {
+    SpannerOptions options =
+        createSpannerOptions().toBuilder().setClientLibToken("pg-adapter").build();
+    try (Spanner spanner = options.getService()) {
+      DatabaseClient databaseClient =
+          spanner.getDatabaseClient(DatabaseId.of("[PROJECT]", "[INSTANCE]", "[DATABASE]"));
+      TransactionRunner runner = databaseClient.readWriteTransaction();
+      runner.run(transaction -> transaction.executeUpdate(UPDATE_FOO_STATEMENT));
+    }
+    Key<String> key = Key.of("x-goog-api-client", Metadata.ASCII_STRING_MARSHALLER);
+    assertTrue(lastSeenHeaders.containsKey(key));
+    assertTrue(
+        lastSeenHeaders.get(key),
+        Objects.requireNonNull(lastSeenHeaders.get(key)).contains("pg-adapter"));
+    assertTrue(
+        lastSeenHeaders.get(key),
+        Objects.requireNonNull(lastSeenHeaders.get(key))
+            .contains(ServiceOptions.getGoogApiClientLibName() + "/"));
   }
 
   private SpannerOptions createSpannerOptions() {
