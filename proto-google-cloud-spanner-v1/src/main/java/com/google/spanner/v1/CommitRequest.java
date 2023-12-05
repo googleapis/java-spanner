8c689a1d07f7e79b142e4b0771db1b67af12919d
rahul yadav <rahulyadavsep92@gmail.com>
2023-12-05 10:11:50 +0530

    add commit delay options

--- proto-google-cloud-spanner-v1/src/main/java/com/google/spanner/v1/CommitRequest.java
+++ proto-google-cloud-spanner-v1/src/main/java/com/google/spanner/v1/CommitRequest.java
@@ -378,6 +378,36 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
     return returnCommitStats_;
   }
 
+  public static final int MAX_COMMIT_DELAY_FIELD_NUMBER = 8;
+  private com.google.protobuf.Duration maxCommitDelay_;
+  /**
+   * <code>.google.protobuf.Duration max_commit_delay = 8;</code>
+   *
+   * @return Whether the maxCommitDelay field is set.
+   */
+  @java.lang.Override
+  public boolean hasMaxCommitDelay() {
+    return maxCommitDelay_ != null;
+  }
+  /**
+   * <code>.google.protobuf.Duration max_commit_delay = 8;</code>
+   *
+   * @return The maxCommitDelay.
+   */
+  @java.lang.Override
+  public com.google.protobuf.Duration getMaxCommitDelay() {
+    return maxCommitDelay_ == null
+        ? com.google.protobuf.Duration.getDefaultInstance()
+        : maxCommitDelay_;
+  }
+  /** <code>.google.protobuf.Duration max_commit_delay = 8;</code> */
+  @java.lang.Override
+  public com.google.protobuf.DurationOrBuilder getMaxCommitDelayOrBuilder() {
+    return maxCommitDelay_ == null
+        ? com.google.protobuf.Duration.getDefaultInstance()
+        : maxCommitDelay_;
+  }
+
   public static final int REQUEST_OPTIONS_FIELD_NUMBER = 6;
   private com.google.spanner.v1.RequestOptions requestOptions_;
   /**
@@ -460,6 +490,9 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
     if (requestOptions_ != null) {
       output.writeMessage(6, getRequestOptions());
     }
+    if (maxCommitDelay_ != null) {
+      output.writeMessage(8, getMaxCommitDelay());
+    }
     getUnknownFields().writeTo(output);
   }
 
@@ -491,6 +524,9 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
     if (requestOptions_ != null) {
       size += com.google.protobuf.CodedOutputStream.computeMessageSize(6, getRequestOptions());
     }
+    if (maxCommitDelay_ != null) {
+      size += com.google.protobuf.CodedOutputStream.computeMessageSize(8, getMaxCommitDelay());
+    }
     size += getUnknownFields().getSerializedSize();
     memoizedSize = size;
     return size;
@@ -509,6 +545,10 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
     if (!getSession().equals(other.getSession())) return false;
     if (!getMutationsList().equals(other.getMutationsList())) return false;
     if (getReturnCommitStats() != other.getReturnCommitStats()) return false;
+    if (hasMaxCommitDelay() != other.hasMaxCommitDelay()) return false;
+    if (hasMaxCommitDelay()) {
+      if (!getMaxCommitDelay().equals(other.getMaxCommitDelay())) return false;
+    }
     if (hasRequestOptions() != other.hasRequestOptions()) return false;
     if (hasRequestOptions()) {
       if (!getRequestOptions().equals(other.getRequestOptions())) return false;
@@ -543,6 +583,10 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
     }
     hash = (37 * hash) + RETURN_COMMIT_STATS_FIELD_NUMBER;
     hash = (53 * hash) + com.google.protobuf.Internal.hashBoolean(getReturnCommitStats());
+    if (hasMaxCommitDelay()) {
+      hash = (37 * hash) + MAX_COMMIT_DELAY_FIELD_NUMBER;
+      hash = (53 * hash) + getMaxCommitDelay().hashCode();
+    }
     if (hasRequestOptions()) {
       hash = (37 * hash) + REQUEST_OPTIONS_FIELD_NUMBER;
       hash = (53 * hash) + getRequestOptions().hashCode();
@@ -709,6 +753,11 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
       }
       bitField0_ = (bitField0_ & ~0x00000008);
       returnCommitStats_ = false;
+      maxCommitDelay_ = null;
+      if (maxCommitDelayBuilder_ != null) {
+        maxCommitDelayBuilder_.dispose();
+        maxCommitDelayBuilder_ = null;
+      }
       requestOptions_ = null;
       if (requestOptionsBuilder_ != null) {
         requestOptionsBuilder_.dispose();
@@ -772,6 +821,10 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
         result.returnCommitStats_ = returnCommitStats_;
       }
       if (((from_bitField0_ & 0x00000020) != 0)) {
+        result.maxCommitDelay_ =
+            maxCommitDelayBuilder_ == null ? maxCommitDelay_ : maxCommitDelayBuilder_.build();
+      }
+      if (((from_bitField0_ & 0x00000040) != 0)) {
         result.requestOptions_ =
             requestOptionsBuilder_ == null ? requestOptions_ : requestOptionsBuilder_.build();
       }
@@ -865,6 +918,9 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
       if (other.getReturnCommitStats() != false) {
         setReturnCommitStats(other.getReturnCommitStats());
       }
+      if (other.hasMaxCommitDelay()) {
+        mergeMaxCommitDelay(other.getMaxCommitDelay());
+      }
       if (other.hasRequestOptions()) {
         mergeRequestOptions(other.getRequestOptions());
       }
@@ -950,9 +1006,15 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
             case 50:
               {
                 input.readMessage(getRequestOptionsFieldBuilder().getBuilder(), extensionRegistry);
-                bitField0_ |= 0x00000020;
+                bitField0_ |= 0x00000040;
                 break;
               } // case 50
+            case 66:
+              {
+                input.readMessage(getMaxCommitDelayFieldBuilder().getBuilder(), extensionRegistry);
+                bitField0_ |= 0x00000020;
+                break;
+              } // case 66
             default:
               {
                 if (!super.parseUnknownField(input, extensionRegistry, tag)) {
@@ -1896,6 +1958,121 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
       return this;
     }
 
+    private com.google.protobuf.Duration maxCommitDelay_;
+    private com.google.protobuf.SingleFieldBuilderV3<
+            com.google.protobuf.Duration,
+            com.google.protobuf.Duration.Builder,
+            com.google.protobuf.DurationOrBuilder>
+        maxCommitDelayBuilder_;
+    /**
+     * <code>.google.protobuf.Duration max_commit_delay = 8;</code>
+     *
+     * @return Whether the maxCommitDelay field is set.
+     */
+    public boolean hasMaxCommitDelay() {
+      return ((bitField0_ & 0x00000020) != 0);
+    }
+    /**
+     * <code>.google.protobuf.Duration max_commit_delay = 8;</code>
+     *
+     * @return The maxCommitDelay.
+     */
+    public com.google.protobuf.Duration getMaxCommitDelay() {
+      if (maxCommitDelayBuilder_ == null) {
+        return maxCommitDelay_ == null
+            ? com.google.protobuf.Duration.getDefaultInstance()
+            : maxCommitDelay_;
+      } else {
+        return maxCommitDelayBuilder_.getMessage();
+      }
+    }
+    /** <code>.google.protobuf.Duration max_commit_delay = 8;</code> */
+    public Builder setMaxCommitDelay(com.google.protobuf.Duration value) {
+      if (maxCommitDelayBuilder_ == null) {
+        if (value == null) {
+          throw new NullPointerException();
+        }
+        maxCommitDelay_ = value;
+      } else {
+        maxCommitDelayBuilder_.setMessage(value);
+      }
+      bitField0_ |= 0x00000020;
+      onChanged();
+      return this;
+    }
+    /** <code>.google.protobuf.Duration max_commit_delay = 8;</code> */
+    public Builder setMaxCommitDelay(com.google.protobuf.Duration.Builder builderForValue) {
+      if (maxCommitDelayBuilder_ == null) {
+        maxCommitDelay_ = builderForValue.build();
+      } else {
+        maxCommitDelayBuilder_.setMessage(builderForValue.build());
+      }
+      bitField0_ |= 0x00000020;
+      onChanged();
+      return this;
+    }
+    /** <code>.google.protobuf.Duration max_commit_delay = 8;</code> */
+    public Builder mergeMaxCommitDelay(com.google.protobuf.Duration value) {
+      if (maxCommitDelayBuilder_ == null) {
+        if (((bitField0_ & 0x00000020) != 0)
+            && maxCommitDelay_ != null
+            && maxCommitDelay_ != com.google.protobuf.Duration.getDefaultInstance()) {
+          getMaxCommitDelayBuilder().mergeFrom(value);
+        } else {
+          maxCommitDelay_ = value;
+        }
+      } else {
+        maxCommitDelayBuilder_.mergeFrom(value);
+      }
+      bitField0_ |= 0x00000020;
+      onChanged();
+      return this;
+    }
+    /** <code>.google.protobuf.Duration max_commit_delay = 8;</code> */
+    public Builder clearMaxCommitDelay() {
+      bitField0_ = (bitField0_ & ~0x00000020);
+      maxCommitDelay_ = null;
+      if (maxCommitDelayBuilder_ != null) {
+        maxCommitDelayBuilder_.dispose();
+        maxCommitDelayBuilder_ = null;
+      }
+      onChanged();
+      return this;
+    }
+    /** <code>.google.protobuf.Duration max_commit_delay = 8;</code> */
+    public com.google.protobuf.Duration.Builder getMaxCommitDelayBuilder() {
+      bitField0_ |= 0x00000020;
+      onChanged();
+      return getMaxCommitDelayFieldBuilder().getBuilder();
+    }
+    /** <code>.google.protobuf.Duration max_commit_delay = 8;</code> */
+    public com.google.protobuf.DurationOrBuilder getMaxCommitDelayOrBuilder() {
+      if (maxCommitDelayBuilder_ != null) {
+        return maxCommitDelayBuilder_.getMessageOrBuilder();
+      } else {
+        return maxCommitDelay_ == null
+            ? com.google.protobuf.Duration.getDefaultInstance()
+            : maxCommitDelay_;
+      }
+    }
+    /** <code>.google.protobuf.Duration max_commit_delay = 8;</code> */
+    private com.google.protobuf.SingleFieldBuilderV3<
+            com.google.protobuf.Duration,
+            com.google.protobuf.Duration.Builder,
+            com.google.protobuf.DurationOrBuilder>
+        getMaxCommitDelayFieldBuilder() {
+      if (maxCommitDelayBuilder_ == null) {
+        maxCommitDelayBuilder_ =
+            new com.google.protobuf.SingleFieldBuilderV3<
+                com.google.protobuf.Duration,
+                com.google.protobuf.Duration.Builder,
+                com.google.protobuf.DurationOrBuilder>(
+                getMaxCommitDelay(), getParentForChildren(), isClean());
+        maxCommitDelay_ = null;
+      }
+      return maxCommitDelayBuilder_;
+    }
+
     private com.google.spanner.v1.RequestOptions requestOptions_;
     private com.google.protobuf.SingleFieldBuilderV3<
             com.google.spanner.v1.RequestOptions,
@@ -1914,7 +2091,7 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
      * @return Whether the requestOptions field is set.
      */
     public boolean hasRequestOptions() {
-      return ((bitField0_ & 0x00000020) != 0);
+      return ((bitField0_ & 0x00000040) != 0);
     }
     /**
      *
@@ -1954,7 +2131,7 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
       } else {
         requestOptionsBuilder_.setMessage(value);
       }
-      bitField0_ |= 0x00000020;
+      bitField0_ |= 0x00000040;
       onChanged();
       return this;
     }
@@ -1973,7 +2150,7 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
       } else {
         requestOptionsBuilder_.setMessage(builderForValue.build());
       }
-      bitField0_ |= 0x00000020;
+      bitField0_ |= 0x00000040;
       onChanged();
       return this;
     }
@@ -1988,7 +2165,7 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
      */
     public Builder mergeRequestOptions(com.google.spanner.v1.RequestOptions value) {
       if (requestOptionsBuilder_ == null) {
-        if (((bitField0_ & 0x00000020) != 0)
+        if (((bitField0_ & 0x00000040) != 0)
             && requestOptions_ != null
             && requestOptions_ != com.google.spanner.v1.RequestOptions.getDefaultInstance()) {
           getRequestOptionsBuilder().mergeFrom(value);
@@ -1998,7 +2175,7 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
       } else {
         requestOptionsBuilder_.mergeFrom(value);
       }
-      bitField0_ |= 0x00000020;
+      bitField0_ |= 0x00000040;
       onChanged();
       return this;
     }
@@ -2012,7 +2189,7 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
      * <code>.google.spanner.v1.RequestOptions request_options = 6;</code>
      */
     public Builder clearRequestOptions() {
-      bitField0_ = (bitField0_ & ~0x00000020);
+      bitField0_ = (bitField0_ & ~0x00000040);
       requestOptions_ = null;
       if (requestOptionsBuilder_ != null) {
         requestOptionsBuilder_.dispose();
@@ -2031,7 +2208,7 @@ public final class CommitRequest extends com.google.protobuf.GeneratedMessageV3
      * <code>.google.spanner.v1.RequestOptions request_options = 6;</code>
      */
     public com.google.spanner.v1.RequestOptions.Builder getRequestOptionsBuilder() {
-      bitField0_ |= 0x00000020;
+      bitField0_ |= 0x00000040;
       onChanged();
       return getRequestOptionsFieldBuilder().getBuilder();
     }
