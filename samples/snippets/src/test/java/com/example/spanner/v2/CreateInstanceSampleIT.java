6eb9f2d928b08dfcb22eeab7825ea53ba1899d9f
Arpan Mishra <arpanmishra@google.com>
2023-12-18 17:00:51 +0530

    chore: adding integration tests for samples.

---
+++ samples/snippets/src/test/java/com/example/spanner/v2/CreateInstanceSampleIT.java
@@ -0,0 +1,9 @@
+package com.example.spanner.v2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CreateInstanceSampleIT extends SampleTestBaseV2 {
+
+}
