06031df8cbccaf4b82237c4755276b729a74f500
Arpan Mishra <arpanmishra@google.com>
2024-04-12 09:51:06 +0530

    chore: fix ITClosedSessionTest.

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/IntegrationTestWithClosedSessionsEnv.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/IntegrationTestWithClosedSessionsEnv.java
@@ -18,6 +18,7 @@ package com.google.cloud.spanner;
 
 import com.google.cloud.spanner.SessionPool.PooledSession;
 import com.google.cloud.spanner.SessionPool.PooledSessionFuture;
+import com.google.cloud.spanner.SessionPool.SessionFuture;
 import com.google.cloud.spanner.testing.RemoteSpannerHelper;
 
 /**
@@ -84,6 +85,12 @@ public class IntegrationTestWithClosedSessionsEnv extends IntegrationTestEnv {
       }
       session.get().setAllowReplacing(allowReplacing);
       return session;
+    }
+
+    @Override
+    SessionFuture getMultiplexedSession() {
+      // TODO when multiplexed session is enabled, this logic needs to change.
+      return getSession();
     }
 
     /**
