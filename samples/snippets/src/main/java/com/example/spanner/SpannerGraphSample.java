b24e3f7ac1fc9e89e40c0e16e3c640d1cf12a639
Bharadwaj V.R. <bharadwajvr@google.com>
2024-07-21 17:52:06 -0700

    Update SpannerGraphSample to align with doc examples already created for gcloud and cli

--- samples/snippets/src/main/java/com/example/spanner/SpannerGraphSample.java
+++ samples/snippets/src/main/java/com/example/spanner/SpannerGraphSample.java
@@ -75,6 +75,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class SpannerGraphSample {
 
+  // [START spanner_insert_graph_data]
   /**
    * Class to contain sample Person data.
    */
@@ -116,6 +117,43 @@ public class SpannerGraphSample {
     }
   }
 
+  /**
+   * Class to contain sample Transfer data.
+   */
+  static class Transfer {
+
+    final long id;
+    final long to_id;
+    final double amount;
+    final Timestamp create_time;
+    final String order_number;
+
+    Transfer(long id, long to_id, double amount, Timestamp create_time, String order_number) {
+      this.id = id;
+      this.to_id = to_id;
+      this.amount = amount;
+      this.create_time = create_time;
+      this.order_number = order_number;
+    }
+  }
+
+  /**
+   * Class to contain sample Ownership data.
+   */
+  static class Own {
+
+    final long id;
+    final long account_id;
+    final Timestamp create_time;
+
+    Own(long id, long account_id, Timestamp create_time) {
+      this.id = id;
+      this.account_id = account_id;
+      this.create_time = create_time;
+    }
+  }
+  // [END spanner_insert_graph_data]
+
   // [START spanner_create_database_with_property_graph]
   static void createDatabaseWithPropertyGraph(DatabaseAdminClient dbAdminClient,
       InstanceName instanceName, String databaseId) {
@@ -150,7 +188,8 @@ public class SpannerGraphSample {
                     + "  id               INT64 NOT NULL,"
                     + "  to_id            INT64 NOT NULL,"
                     + "  amount           FLOAT64,"
-                    + "  create_time      TIMESTAMP NOT NULL,"
+                    + "  create_time      TIMESTAMP NOT NULL OPTIONS"
+                    + "    (allow_commit_timestamp=true),"
                     + "  order_number     STRING(MAX),"
                     + "  FOREIGN KEY (to_id) REFERENCES Account (id)"
                     + ") PRIMARY KEY (id, to_id, create_time),"
@@ -184,20 +223,50 @@ public class SpannerGraphSample {
   // [END spanner_create_database_with_property_graph]
 
   // [START spanner_insert_graph_data]
-  static final List<Person> PERSONS =
-      Arrays.asList(
-          new Person(1, "Izumi", "F", Timestamp.parseTimestamp("2001-01-05T00:00:00.18Z"), "USA", "Mountain View"),
-          new Person(2, "Tal", "M", Timestamp.parseTimestamp("2000-06-22T00:00:00.12Z"), "FR", "Paris"));
-
   static final List<Account> ACCOUNTS =
       Arrays.asList(
-          new Account(1, Timestamp.parseTimestamp("2014-09-27T11:17:42.18Z"), false, "Savings"),
-          new Account(2, Timestamp.parseTimestamp("2008-07-11T12:30:00.12Z"), false, "Checking"));
+          new Account(7, Timestamp.parseTimestamp("2020-01-10T06:22:20.12Z"), false, "Vacation Fund"),
+          new Account(16, Timestamp.parseTimestamp("2020-01-27T17:55:09.12Z"), true, "Vacation Fund"),
+          new Account(20, Timestamp.parseTimestamp("2020-02-18T05:44:20.12Z"), false, "Rainy Day Fund"));
+
+  static final List<Person> PERSONS =
+      Arrays.asList(
+          new Person(1, "Alex", "male", Timestamp.parseTimestamp("1991-12-21T00:00:00.12Z"), "Australia"," Adelaide"),
+          new Person(2, "Dana", "female", Timestamp.parseTimestamp("1980-10-31T00:00:00.12Z"),"Czech_Republic", "Moravia"),
+          new Person(3, "Lee", "male", Timestamp.parseTimestamp("1986-12-07T00:00:00.12Z"), "India", "Kollam"));
+
+  static final List<Transfer> TRANSFERS =
+      Arrays.asList(
+        new Transfer(7, 16, 300.0, Timestamp.parseTimestamp("2020-08-29T15:28:58.12Z"), "304330008004315"),
+        new Transfer(7, 16, 100.0, Timestamp.parseTimestamp("2020-10-04T16:55:05.12Z"), "304120005529714"),
+        new Transfer(16, 20, 300.0, Timestamp.parseTimestamp("2020-09-25T02:36:14.12Z"), "103650009791820"),
+        new Transfer(20, 7, 500.0, Timestamp.parseTimestamp("2020-10-04T16:55:05.12Z"), "304120005529714"),
+        new Transfer(20, 16, 200.0, Timestamp.parseTimestamp("2020-10-17T03:59:40.12Z"), "302290001255747"));
+
+  static final List<Own> OWNERSHIPS =
+      Arrays.asList(
+        new Own(1, 7, Timestamp.parseTimestamp("2020-01-10T06:22:20.12Z")),
+        new Own(2, 20, Timestamp.parseTimestamp("2020-01-27T17:55:09.12Z")),
+        new Own(3, 16, Timestamp.parseTimestamp("2020-02-18T05:44:20.12Z")));
+
   // [END spanner_insert_graph_data]
 
   // [START spanner_insert_graph_data]
   static void insertData(DatabaseClient dbClient) {
     List<Mutation> mutations = new ArrayList<>();
+    for (Account account: ACCOUNTS) {
+      mutations.add(
+          Mutation.newInsertBuilder("Account")
+              .set("id")
+              .to(account.id)
+              .set("create_time")
+              .to(account.create_time)
+              .set("is_blocked")
+              .to(account.is_blocked)
+              .set("nick_name")
+              .to(account.nick_name)
+              .build());
+    }
     for (Person person : PERSONS) {
       mutations.add(
           Mutation.newInsertBuilder("Person")
@@ -215,19 +284,33 @@ public class SpannerGraphSample {
               .to(person.city)
               .build());
     }
-    for (Account account: ACCOUNTS) {
+    for (Transfer transfer: TRANSFERS) {
       mutations.add(
-          Mutation.newInsertBuilder("Account")
+          Mutation.newInsertBuilder("AccountTransferAccount")
               .set("id")
-              .to(account.id)
+              .to(transfer.id)
+              .set("to_id")
+              .to(transfer.to_id)
+              .set("amount")
+              .to(transfer.amount)
               .set("create_time")
-              .to(account.create_time)
-              .set("is_blocked")
-              .to(account.is_blocked)
-              .set("nick_name")
-              .to(account.nick_name)
+              .to(transfer.create_time)
+              .set("order_number")
+              .to(transfer.order_number)
               .build());
     }
+    for (Own own: OWNERSHIPS) {
+      mutations.add(
+          Mutation.newInsertBuilder("PersonOwnAccount")
+              .set("id")
+              .to(own.id)
+              .set("account_id")
+              .to(own.account_id)
+              .set("create_time")
+              .to(own.create_time)
+              .build());
+    }
+
     dbClient.write(mutations);
   }
   // [END spanner_insert_graph_data]
@@ -238,12 +321,12 @@ public class SpannerGraphSample {
         .readWriteTransaction()
         .run(transaction -> {
           String sql =
-              "INSERT INTO PersonOwnAccount (id, account_id, create_time) "
-                  + " VALUES"
-                  + "   (1, 1, '2014-09-28T09:23:31.45Z'),"
-                  + "   (2, 2, '2008-07-12T04:31:18.16Z')";
+              "INSERT INTO Account (id, create_time, is_blocked) "
+              + "  VALUES"
+              + "    (1, CAST('2000-08-10 08:18:48.463959-07:52' AS TIMESTAMP), false),"
+              + "    (2, CAST('2000-08-12 08:18:48.463959-07:52' AS TIMESTAMP), true)";
           long rowCount = transaction.executeUpdate(Statement.of(sql));
-          System.out.printf("%d record inserted into PersonOwnAccount.\n", rowCount);
+          System.out.printf("%d record inserted into Account.\n", rowCount);
           return null;
         });
 
@@ -251,16 +334,58 @@ public class SpannerGraphSample {
         .readWriteTransaction()
         .run(transaction -> {
           String sql =
-              "INSERT INTO AccountTransferAccount (id, to_id, amount, create_time, order_number) "
-                  + " VALUES"
-                  + "   (1, 2, 900, '2024-06-24T10:11:31.26Z', '3LXCTB'),"
-                  + "   (2, 1, 100, '2024-07-01T12:23:28.11Z', '4MYRTQ')";
+              "INSERT INTO AccountTransferAccount (id, to_id, create_time, amount) "
+              +  "  VALUES"
+              +  "    (1, 2, PENDING_COMMIT_TIMESTAMP(), 100),"
+              +  "    (1, 1, PENDING_COMMIT_TIMESTAMP(), 200) ";
           long rowCount = transaction.executeUpdate(Statement.of(sql));
           System.out.printf("%d record inserted into AccountTransferAccount.\n", rowCount);
           return null;
         });
   }
   // [END spanner_insert_graph_data_with_dml]
+
+    // [START spanner_update_graph_data_with_dml]
+  static void updateUsingDml(DatabaseClient dbClient) {
+    dbClient
+        .readWriteTransaction()
+        .run(transaction -> {
+          String sql =
+              "UPDATE Account SET is_blocked = false WHERE id = 2";
+          long rowCount = transaction.executeUpdate(Statement.of(sql));
+          System.out.printf("%d Account record(s) updated.\n", rowCount);
+          return null;
+        });
+
+    dbClient
+        .readWriteTransaction()
+        .run(transaction -> {
+          String sql =
+              "UPDATE AccountTransferAccount SET amount = 300 WHERE id = 1 AND to_id = 2";
+          long rowCount = transaction.executeUpdate(Statement.of(sql));
+          System.out.printf("%d AccountTransferAccount record(s) updated.\n", rowCount);
+          return null;
+        });
+  }
+  // [END spanner_update_graph_data_with_dml]
+
+    // [START spanner_update_graph_data_with_graph_query_in_dml]
+  static void updateUsingGraphQueryInDml(DatabaseClient dbClient) {
+    dbClient
+        .readWriteTransaction()
+        .run(transaction -> {
+          String sql =
+              "UPDATE Account SET is_blocked = true "
+              +  "WHERE id IN ("
+              +  "  GRAPH FinGraph"
+              +  "  MATCH (a:Account WHERE a.id = 1)-[:TRANSFERS]->{1,2}(b:Account)"
+              +  "  RETURN b.id)";
+          long rowCount = transaction.executeUpdate(Statement.of(sql));
+          System.out.printf("%d record updated.\n", rowCount);
+          return null;
+        });
+  }
+  // [END spanner_update_graph_data_with_graph_query_in_dml]
 
   // [START spanner_query_graph_data]
   static void query(DatabaseClient dbClient) {
@@ -284,13 +409,13 @@ public class SpannerGraphSample {
   }
   // [END spanner_query_graph_data]
 
-  // [START spanner_query_with_parameter]
+  // [START spanner_query_graph_data_with_parameter]
   static void queryWithParameter(DatabaseClient dbClient) {
     Statement statement =
         Statement.newBuilder(
             "Graph FinGraph "
             + "MATCH (a:Person)-[o:Owns]->()-[t:Transfers]->()<-[p:Owns]-(b:Person) "
-            + "WHERE t.amount > @min " 
+            + "WHERE t.amount >= @min " 
             + "RETURN a.name AS sender, b.name AS receiver, t.amount, t.create_time AS transfer_at")
             .bind("min")
             .to(500)
@@ -306,49 +431,65 @@ public class SpannerGraphSample {
       }
     }
   }
-  // [END spanner_query_with_parameter]
+  // [END spanner_query_graph_data_with_parameter]
+
+  // [START spanner_delete_graph_data_with_dml]
+  static void deleteUsingDml(DatabaseClient dbClient) {
+    dbClient
+        .readWriteTransaction()
+        .run(transaction -> {
+          String sql = "DELETE FROM AccountTransferAccount WHERE id = 1 AND to_id = 2";
+          long rowCount = transaction.executeUpdate(Statement.of(sql));
+          System.out.printf("%d AccountTransferAccount record(s) deleted.\n", rowCount);
+          return null;
+        });
+
+    dbClient
+        .readWriteTransaction()
+        .run(transaction -> {
+          String sql = "DELETE FROM Account WHERE id = 2";
+          long rowCount = transaction.executeUpdate(Statement.of(sql));
+          System.out.printf("%d Account record(s) deleted.\n", rowCount);
+          return null;
+        });
+  }
+  // [END spanner_delete_graph_data_with_dml]
 
   // [START spanner_delete_data]
-  static void deleteExampleData(DatabaseClient dbClient) {
+  static void deleteData(DatabaseClient dbClient) {
     List<Mutation> mutations = new ArrayList<>();
 
     // KeySet.Builder can be used to delete a specific set of rows.
-    // Delete the Albums with the key values (1,1) and (2,2).
+    // Delete the PersonOwnAccount rows with the key values (1,7) and (2,20).
     mutations.add(
         Mutation.delete(
-            "PersonOwnAccount", KeySet.newBuilder().addKey(Key.of(1, 1)).addKey(Key.of(2, 2)).build()));
+            "PersonOwnAccount",
+            KeySet.newBuilder()
+                .addKey(Key.of(1, 7))
+                .addKey(Key.of(2, 20))
+                .build()));
 
     // KeyRange can be used to delete rows with a key in a specific range.
-    // Delete a range of rows where the column key is >=1 and <3
+    // Delete a range of rows where the key prefix is >=1 and <8
     mutations.add(
-        Mutation.delete("AccountTransferAccount", KeySet.range(KeyRange.closedOpen(Key.of(1), Key.of(3)))));
+        Mutation.delete(
+            "AccountTransferAccount",
+            KeySet.range(KeyRange.closedOpen(Key.of(1), Key.of(8)))));
+
+    // KeySet.all() can be used to delete all the rows in a table.
+    // Delete all Account rows, which will also delete the remaining
+    // AccountTransferAccount rows since it was defined with ON DELETE CASCADE.
+    mutations.add(Mutation.delete("Account", KeySet.all()));
+
+    // KeySet.all() can be used to delete all the rows in a table.
+    // Delete all Person rows, which will also delete the remaining
+    // PersonOwnAccount rows since it was defined with ON DELETE CASCADE.
+    mutations.add(Mutation.delete("Person", KeySet.all()));
 
     dbClient.write(mutations);
     System.out.printf("Records deleted.\n");
   }
   // [END spanner_delete_data]
-
-  // [START spanner_dml_standard_delete]
-  static void deleteUsingDml(DatabaseClient dbClient) {
-    dbClient
-        .readWriteTransaction()
-        .run(transaction -> {
-          String sql = "DELETE FROM Account AS a WHERE EXTRACT(YEAR FROM DATE(a.create_time)) >= 2000";
-          long rowCount = transaction.executeUpdate(Statement.of(sql));
-          System.out.printf("%d record deleted from Account.\n", rowCount);
-          return null;
-        });
-
-    dbClient
-        .readWriteTransaction()
-        .run(transaction -> {
-          String sql = "DELETE FROM Person WHERE True";
-          long rowCount = transaction.executeUpdate(Statement.of(sql));
-          System.out.printf("%d record deleted from Person.\n", rowCount);
-          return null;
-        });
-  }
-  // [END spanner_dml_standard_delete]
 
   static void run(
       DatabaseClient dbClient,
@@ -366,17 +507,23 @@ public class SpannerGraphSample {
       case "insertusingdml":
         insertUsingDml(dbClient);
         break;
+      case "updateusingdml":
+        updateUsingDml(dbClient);
+        break;
+      case "updateusinggraphqueryindml":
+        updateUsingGraphQueryInDml(dbClient);
+        break;
       case "query":
         query(dbClient);
         break;
       case "querywithparameter":
         queryWithParameter(dbClient);
         break;
-      case "delete":
-        deleteExampleData(dbClient);
-        break;
       case "deleteusingdml":
         deleteUsingDml(dbClient);
+        break;
+      case "delete":
+        deleteData(dbClient);
         break;
       default:
         printUsageAndExit();
@@ -391,10 +538,12 @@ public class SpannerGraphSample {
     System.err.println("    SpannerGraphExample createdatabase my-instance example-db");
     System.err.println("    SpannerGraphExample insert my-instance example-db");
     System.err.println("    SpannerGraphExample insertusingdml my-instance example-db");
+    System.err.println("    SpannerGraphExample updateusingdml my-instance example-db");
+    System.err.println("    SpannerGraphExample updateusinggraphqueryindml my-instance example-db");
     System.err.println("    SpannerGraphExample query my-instance example-db");
     System.err.println("    SpannerGraphExample querywithparameter my-instance example-db");
-    System.err.println("    SpannerGraphExample delete my-instance example-db");
     System.err.println("    SpannerGraphExample deleteusingdml my-instance example-db");
+    System.err.println("    SpannerGraphExample delete my-instance example-db");
     System.exit(1);
   }
 
