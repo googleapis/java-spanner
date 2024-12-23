d7385c3e79f326444c6c013e500961be241a1ecc
Sri Harsha CH <sriharshach@google.com>
2024-12-23 14:47:35 +0000

    chore(spanner): unskip tests

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -130,7 +130,6 @@ integration-multiplexed-sessions-enabled)
       -Dmaven.main.skip=true \
       -Dspanner.gce.config.project_id=gcloud-devel \
       -Dspanner.testenv.instance=projects/gcloud-devel/instances/java-client-integration-tests-multiplexed-sessions \
-      -DskipTests=true \
       -fae \
       verify
     RETURN_CODE=$?
