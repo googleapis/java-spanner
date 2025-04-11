d358cb96e33bdf6de6528d03c884aa702b40b802
Knut Olav Løite <koloite@gmail.com>
2025-04-11 11:19:43 +0200

    perf: qualify statements without removing comments (#3810)
    * perf: qualify statements without removing comments
    
    Determine the type of statement without first removing all comments and hints.
    This prevents the creation of new strings and stepping through the entire SQL
    string for each statement that is not found in the statement cache.
    
    Benchmark                                Mode  Cnt       Score      Error  Units
    StatementParserBenchmark.isQueryTest    thrpt    5  547904.501 ± 1970.170  ops/s
    StatementParserBenchmark.longDmlTest    thrpt    5  114806.782 ±  826.881  ops/s
    StatementParserBenchmark.longQueryTest  thrpt    5  112666.992 ±  700.783  ops/s
    
    * chore: generate libraries at Fri Apr 11 05:56:44 UTC 2025
    
    ---------
    
    Co-authored-by: cloud-java-bot <cloud-java-bot@google.com>

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/connection/StatementParserTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/connection/StatementParserTest.java
@@ -17,7 +17,6 @@
 package com.google.cloud.spanner.connection;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotSame;
