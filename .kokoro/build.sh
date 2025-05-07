513b6971a64d43022119dab48dce5d1b3f51a667
Lawrence Qiu <lawrenceqiu@google.com>
2025-05-07 14:30:01 -0400

    chore: Run with v4.29.4

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -92,6 +92,7 @@ javadoc)
 integration)
     mvn -B ${INTEGRATION_TEST_ARGS} \
       -ntp \
+      -Dprotobuf.version=4.29.4 \
       -Penable-integration-tests \
       -Djava.net.preferIPv4Stack=true \
       -DtrimStackTrace=false \
