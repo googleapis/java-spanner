2d90f42476bd8d06ce9ad0cc309945ffb2a7ac72
Lawrence Qiu <lawrenceqiu@google.com>
2025-05-27 10:19:19 -0400

    chore: Update to Protobuf v4.31.0

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -92,7 +92,7 @@ javadoc)
 integration)
     mvn -B ${INTEGRATION_TEST_ARGS} \
       -ntp \
-      -Dprotobuf.version=4.31.0-RC1 \
+      -Dprotobuf.version=4.31.0 \
       -Penable-integration-tests \
       -Djava.net.preferIPv4Stack=true \
       -DtrimStackTrace=false \
