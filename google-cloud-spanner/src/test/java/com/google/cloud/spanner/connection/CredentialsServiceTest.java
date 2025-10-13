94d0474ace62ea1059e5b69243f0b6eef31ddd06
Lawrence Qiu <lawrenceqiu@google.com>
2025-10-13 19:21:06 +0000

    fix: Migrate away from GoogleCredentials.fromStream() usages (#4151)
    * fix: Migrate away from GoogleCredentials.fromStream() usages
    
    * chore: generate libraries at Wed Oct  8 17:59:33 UTC 2025
    
    * chore: generate libraries at Fri Oct 10 04:46:45 UTC 2025
    
    * chore: Resolve issue with Paths.get()
    
    * chore: Add comment for fixed test
    
    ---------
    
    Co-authored-by: cloud-java-bot <cloud-java-bot@google.com>
    Co-authored-by: Sakthivel Subramanian <179120858+sakthivelmanii@users.noreply.github.com>
    Co-authored-by: rahul2393 <irahul@google.com>

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/connection/CredentialsServiceTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/connection/CredentialsServiceTest.java
@@ -25,8 +25,9 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.spanner.ErrorCode;
 import com.google.cloud.spanner.SpannerException;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -35,9 +36,9 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class CredentialsServiceTest {
   private static final String FILE_TEST_PATH =
-      CredentialsServiceTest.class.getResource("test-key.json").getFile();
-  private static final String APP_DEFAULT_FILE_TEST_PATH =
-      CredentialsServiceTest.class.getResource("test-key-app-default.json").getFile();
+      CredentialsServiceTest.class.getResource("test-key.json").getPath();
+  private static final String SA_APP_DEFAULT_FILE_TEST_PATH =
+      CredentialsServiceTest.class.getResource("test-key-app-default.json").getPath();
 
   private static final String TEST_PROJECT_ID = "test-project";
   private static final String APP_DEFAULT_PROJECT_ID = "app-default-test-project";
@@ -49,7 +50,11 @@ public class CredentialsServiceTest {
         GoogleCredentials internalGetApplicationDefault() throws IOException {
           // Read application default credentials directly from a specific file instead of actually
           // fetching the default from the environment.
-          return GoogleCredentials.fromStream(new FileInputStream(APP_DEFAULT_FILE_TEST_PATH));
+          return ServiceAccountCredentials.fromStream(
+              // Calling `getResource().getPath()` on Windows returns a string that might start with
+              // something like `/C:/...`. Paths.get() interprets the leading / as part of the path
+              // and would be invalid. Use `new File().toPath()` to read from these files.
+              Files.newInputStream(new File(SA_APP_DEFAULT_FILE_TEST_PATH).toPath()));
         }
       };
 
