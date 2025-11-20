5874f8b3e65adc3e78832866ebe667cd746e2d7f
Knut Olav Løite <koloite@gmail.com>
2025-11-20 15:20:38 +0100

    feat: include PostgreSQL error code in exceptions (#4236)
    * feat: include PostgreSQL error code in exceptions
    
    PostgreSQL error codes are now included in SpannerException when available.
    
    The PostgreSQL error code is only filled with a non-null value if the database is a
    Spanner PostgreSQL-dialect database, and the error is one that has a corresponding
    error code in PostgreSQL.
    
    See https://www.postgresql.org/docs/current/errcodes-appendix.html for a full list
    of all PostgreSQL error codes.
    
    * chore: generate libraries at Wed Nov 19 17:51:21 UTC 2025
    
    * chore: fix formatting and tests
    
    ---------
    
    Co-authored-by: cloud-java-bot <cloud-java-bot@google.com>

--- google-cloud-spanner/src/main/java/com/google/cloud/spanner/GrpcResultSet.java
+++ google-cloud-spanner/src/main/java/com/google/cloud/spanner/GrpcResultSet.java
@@ -16,7 +16,7 @@
 
 package com.google.cloud.spanner;
 
-import static com.google.cloud.spanner.SpannerExceptionFactory.newSpannerException;
+import static com.google.cloud.spanner.SpannerExceptionFactory.asSpannerException;
 import static com.google.common.base.Preconditions.checkState;
 
 import com.google.api.core.InternalApi;
@@ -76,7 +76,7 @@ class GrpcResultSet extends AbstractResultSet<List<Object>>
   @Override
   public boolean next() throws SpannerException {
     if (error != null) {
-      throw newSpannerException(error);
+      throw asSpannerException(error);
     }
     try {
       if (currRow == null) {
@@ -108,7 +108,7 @@ class GrpcResultSet extends AbstractResultSet<List<Object>>
       return hasNext;
     } catch (Throwable t) {
       throw yieldError(
-          SpannerExceptionFactory.asSpannerException(t),
+          asSpannerException(t),
           iterator.isWithBeginTransaction() && currRow == null,
           iterator.isLastStatement());
     }
