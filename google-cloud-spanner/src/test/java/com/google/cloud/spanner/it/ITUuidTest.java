bd6b703a0fa2e3cc3d423f657628e930c4de7d59
Gagan Gupta <gagangupt16@gmail.com>
2026-01-19 12:27:40 +0530

    chore: Remove unused `isNullOrEmpty` import from `ITUuidTest.java`.

--- google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITUuidTest.java
+++ google-cloud-spanner/src/test/java/com/google/cloud/spanner/it/ITUuidTest.java
@@ -16,7 +16,6 @@
 
 package com.google.cloud.spanner.it;
 
-import static com.google.common.base.Strings.isNullOrEmpty;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
